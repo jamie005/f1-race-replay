3455df9be5bfc68a1a14b32dedf193325b9f47e9
Jamie <jamiehuff2@gmail.com>
2025-04-27 18:35:27 +0000

    Inital commit

---
+++ apps/f1-race-viewer/vite.config.ts
@@ -0,0 +1,40 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => ({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/f1-race-viewer',
+  server: {
+    port: 4200,
+    host: '127.0.0.1',
+  },
+  preview: {
+    port: 4300,
+    host: '127.0.0.1',
+  },
+  plugins: [react()],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  test: {
+    watch: false,
+    globals: true,
+    environment: 'jsdom',
+    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: './test-output/vitest/coverage',
+      provider: 'v8' as const,
+    },
+  },
+}));
