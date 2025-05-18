8f7289a1bca024f818df8e5a094b76661a26a2f8
jamie005 <jamiehuff2@gmail.com>
2025-05-18 14:30:53 +0000

    Refactor import order and implemented transformation of additional car telmetry data

--- apps/f1-car-data-stub/f1_car_data_stub/transformers/telemetry_transformer.py
+++ apps/f1-car-data-stub/f1_car_data_stub/transformers/telemetry_transformer.py
@@ -1,8 +1,8 @@
-from f1_car_data_stub.geometry.wgs84_position import Wgs84Position
 import pandas as pd
+import pymap3d
 
 from f1_replay_data_model_py.F1CarTelemetryReport_pb2 import F1CarTelemeteryReport
-import pymap3d
+from f1_car_data_stub.geometry.wgs84_position import Wgs84Position
 
 
 class TelemetryTransformer():
@@ -18,4 +18,15 @@ class TelemetryTransformer():
         f1_car_telemetry_report = F1CarTelemeteryReport()
         f1_car_telemetry_report.latitude = latitude
         f1_car_telemetry_report.longitude = longitude
+        f1_car_telemetry_report.engine_rpm = telemetry["RPM"]
+        f1_car_telemetry_report.speed_kmh = telemetry["Speed"]
+        f1_car_telemetry_report.gear = telemetry["nGear"]
+        f1_car_telemetry_report.engine_rpm = telemetry["Throttle"]
+        f1_car_telemetry_report.brake_on = telemetry["Brake"]
+
+        if telemetry["Status"] == "OnTrack":
+            f1_car_telemetry_report.on_track = True
+        elif telemetry["Status"] == "OffTrack":
+            f1_car_telemetry_report.on_track = False
+
         return f1_car_telemetry_report
