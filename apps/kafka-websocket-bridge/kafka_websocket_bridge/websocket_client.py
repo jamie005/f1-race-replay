165323bb7dbfe0be7ce62e55e64fc091aa4f34d4
jamie005 <jamiehuff2@gmail.com>
2025-05-28 22:38:58 +0000

    Add websocket client implementation for Kafka message logging

--- apps/kafka-websocket-bridge/kafka_websocket_bridge/websocket_client.py
+++ apps/kafka-websocket-bridge/kafka_websocket_bridge/websocket_client.py
@@ -2,18 +2,19 @@ import asyncio
 import logging
 from websockets.asyncio.client import connect
 
+from f1_replay_data_model_py.F1CarTelemetryReport_pb2 import F1CarTelemetryReport
 from f1_replay_py_helpers.logging import color_log_handler
 
 
 async def main():
-        # Default WARNING level logger setup
     logger = logging.getLogger(__package__)
     logger.addHandler(color_log_handler)
-    logger.setLevel(logging.INFO)  # Set to INFO for more detailed output
+    logger.setLevel(logging.INFO)
 
     async with connect("ws://localhost:8765") as websocket:
         async for message in websocket:
-            logger.info("Received a message!")
+            logger.info(f"Received a message:\n{F1CarTelemetryReport.FromString(message)}")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
