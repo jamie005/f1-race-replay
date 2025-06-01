8290319fc118fa0b43c007216c85c6fce627aa0a
jamie005 <jamiehuff2@gmail.com>
2025-06-01 16:55:41 +0000

    Add react-use-websocket dependency and implement WebSocket handling in f1 race viewer

--- apps/f1-race-viewer/src/app/app.tsx
+++ apps/f1-race-viewer/src/app/app.tsx
@@ -1,6 +1,25 @@
+import { useEffect} from 'react';
 import { Viewer } from "resium";
+import useWebSocket from 'react-use-websocket';
+import { F1CarTelemetryReport } from '@f1-race-replay/data-model'
 
 export function App() {
+  const { lastMessage, readyState, getWebSocket } = useWebSocket("ws://127.0.0.1:8765");
+  
+  useEffect(() => {
+    if (!getWebSocket()) return;
+    getWebSocket().binaryType = 'arraybuffer';
+  }, [getWebSocket, readyState]);
+
+  useEffect(() => {
+    if (!lastMessage) return;
+    if (!(lastMessage.data instanceof ArrayBuffer)) return;
+
+    console.log("Received message:", F1CarTelemetryReport.decode(new Uint8Array(lastMessage.data)));
+    
+    
+  }, [lastMessage]);
+
   return (
     <Viewer full></Viewer>
   );
