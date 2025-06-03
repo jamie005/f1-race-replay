930ba26fdf9d7a38926cdb19cbf33a9f7c8a1a44
jamie005 <jamiehuff2@gmail.com>
2025-06-03 19:25:00 +0000

    Switched from linear interpolation to LagrangePolynomialApproximation so the turning circle of the car appears smooth. Added inertial tracking so the camera has a more car centric approach. Added a temporary 3d model so the heading of the car can be seen

--- apps/f1-race-viewer/src/app/app.tsx
+++ apps/f1-race-viewer/src/app/app.tsx
@@ -1,24 +1,35 @@
 import { useEffect, useRef } from 'react';
-import { Entity, Viewer, Clock } from "resium";
+import { Entity, Viewer, Clock, ModelGraphics } from 'resium';
 import useWebSocket from 'react-use-websocket';
-import { F1CarTelemetryReport } from '@f1-race-replay/data-model'
-import { Cartesian3, Color, ExtrapolationType, JulianDate, SampledPositionProperty, LagrangePolynomialApproximation, LinearApproximation, VelocityOrientationProperty, TrackingReferenceFrame } from 'cesium';
+import { F1CarTelemetryReport } from '@f1-race-replay/data-model';
+import {
+  Cartesian3,
+  Color,
+  ExtrapolationType,
+  JulianDate,
+  SampledPositionProperty,
+  LagrangePolynomialApproximation,
+  VelocityOrientationProperty,
+  TrackingReferenceFrame,
+} from 'cesium';
 
 export function App() {
-  const { lastMessage, readyState, getWebSocket } = useWebSocket("ws://127.0.0.1:8765");
+  const { lastMessage, readyState, getWebSocket } = useWebSocket(
+    'ws://127.0.0.1:8765'
+  );
   const lastReport = useRef<F1CarTelemetryReport | null>(null);
   const sampledPositionProperty = useRef<SampledPositionProperty>(
     new SampledPositionProperty()
   );
-  
+
   // Set interpolation options once
   useEffect(() => {
-    sampledPositionProperty.current.forwardExtrapolationType = ExtrapolationType.HOLD;
+    sampledPositionProperty.current.forwardExtrapolationType =
+      ExtrapolationType.HOLD;
     sampledPositionProperty.current.forwardExtrapolationDuration = 10;
-    // sampledPositionProperty.current.backwardExtrapolationType = 
     sampledPositionProperty.current.setInterpolationOptions({
-      interpolationDegree: 20,
-      interpolationAlgorithm: LinearApproximation,
+      interpolationDegree: 8,
+      interpolationAlgorithm: LagrangePolynomialApproximation,
     });
   }, []);
 
@@ -31,36 +42,43 @@ export function App() {
     if (!lastMessage) return;
     if (!(lastMessage.data instanceof ArrayBuffer)) return;
 
-    const report: F1CarTelemetryReport = F1CarTelemetryReport.decode(new Uint8Array(lastMessage.data))
-    console.log("Received message:", report);
+    const report: F1CarTelemetryReport = F1CarTelemetryReport.decode(
+      new Uint8Array(lastMessage.data)
+    );
+    console.log('Received message:', report);
     lastReport.current = report;
     sampledPositionProperty.current.addSample(
       JulianDate.now(),
       Cartesian3.fromDegrees(report.longitude, report.latitude, 0)
     );
-
-
   }, [lastMessage]);
 
   return (
     <Viewer full>
-      <Clock shouldAnimate/>
+      <Clock shouldAnimate />
       <Entity
         key={lastReport.current?.driver}
         position={sampledPositionProperty.current}
-        orientation={new VelocityOrientationProperty(sampledPositionProperty.current)}
+        orientation={
+          new VelocityOrientationProperty(sampledPositionProperty.current)
+        }
         point={{ pixelSize: 10, color: Color.RED }}
-        description={lastReport.current ? 
-          `
+        trackingReferenceFrame={TrackingReferenceFrame.INERTIAL}
+        description={
+          lastReport.current
+            ? `
           Driver: ${lastReport.current.driver} <br/>
           Speed: ${lastReport.current.speedKmh} km/h <br/>
           Engine RPM: ${lastReport.current.engineRpm} <br/>
           Gear: ${lastReport.current.gear} <br/>
           Throttle Actuation: ${lastReport.current.throttlePercent}% <br/>
           Brake Actuation: ${lastReport.current.brakeOn}
-          ` 
-          : "No data yet"}
-      />
+          `
+            : 'No data yet'
+        }
+      >
+        <ModelGraphics uri={'./src/assets/CesiumMilkTruck.glb'} />
+      </Entity>
     </Viewer>
   );
 }
